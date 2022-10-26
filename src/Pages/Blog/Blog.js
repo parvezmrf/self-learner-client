11e460d400ab51e40118b8a94ced3a37a78a0b8a
unknown <parvezmrf@gmail.com>
2022-10-26 17:30:42 +0600

    update blog

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -30,13 +30,11 @@ const Blog = () => {
                     <Accordion.Header>What is node? What does work?</Accordion.Header>
                     <Accordion.Body>
                         Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node.js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
-
                         Working of Node.js: Node.js accepts the request from the clients and sends the response, while working with the request node.js handles them with a single thread. To operate I/O operations or requests node.js use the concept of threads. Thread is a sequence of instructions that the server needs to perform. It runs parallel on the server to provide the information to multiple clients. Node.js is an event loop single-threaded language. It can handle concurrent requests with a single thread without blocking it for one request.
+                        Node.js basically works on two concept.<br />
 
-                        Node.js basically works on two concept
-
-                        Asynchronous
-                        Non-blocking I/O
+                        Asynchronous<br />
+                        Non-blocking I/O <br />
                         Non-blocking I/o: Non-blocking i/o  means working with multiple requests without blocking the thread for a single request. I/O basically interacts with external systems such as files, databases. Node.js is not used for CPU-intensive work means for calculations, video processing because a single thread cannot handle the CPU works.
                     </Accordion.Body>
                 </Accordion.Item>
